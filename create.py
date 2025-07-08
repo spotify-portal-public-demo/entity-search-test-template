0602c9cbbcc267f536cb03563c528c975b780fc5
Paul Frybarger <pfrybarger@spotify.com>
2025-07-08 11:11:53 +0100

    initial commit

---
+++ create.py
@@ -0,0 +1,24 @@
+num_entities = 500
+
+entity = """
+apiVersion: backstage.io/v1alpha1
+kind: Resource
+metadata:
+  name: my-db-{num}
+  tags:
+    - gcp
+    - cloud-sql
+  annotations:
+    backstage.io/techdocs-ref: dir:.
+    github.com/project-slug: spotify-portal-public-demo/entity-search-test-template
+spec:
+  type: database
+  lifecycle: experimental
+  owner: group:default/github
+"""
+
+entities = []
+for num in range(num_entities):
+    entities.append(entity.format(num=num))
+
+print("---".join(entities))
